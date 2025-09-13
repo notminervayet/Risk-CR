d5d162af0d217629abd4c449f54e78c3e8cd0245
EYTHAN YOHEL MENDEZ RODRIGUEZ <yoelr086@gmail.com>
2025-09-12 21:03:33 -0600

    Creacion de MapaFormulario.cs

---
+++ Risk CR/MapaFormulario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Risk_CR
+{
+    public partial class MapaFormulario : Form
+    {
+        public MapaFormulario()
+        {
+            InitializeComponent();
+
+        }
+    }
+}
