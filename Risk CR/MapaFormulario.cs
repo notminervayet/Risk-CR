4d42e2db282f10ce960c76f6caf772e2f2fcf50d
EYTHAN YOHEL MENDEZ RODRIGUEZ <yoelr086@gmail.com>
2025-09-13 13:23:43 -0600

    boton para ir a mapa formulario, mapaformulario con ventana bien hecha, y mapa puesto

--- Risk CR/MapaFormulario.cs
+++ Risk CR/MapaFormulario.cs
@@ -7,15 +7,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.IO;
 namespace Risk_CR
 {
     public partial class MapaFormulario : Form
     {
+        private PictureBox picMapa;
         public MapaFormulario()
         {
             InitializeComponent();
+            ConfigurarVentana();
+            InicializarMapa();
+        }
 
+        private void ConfigurarVentana()
+        {
+            this.Text = "Risk Costa Rica";
+            this.Size = new Size(1200, 800);
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.BackColor = Color.Black;
+        }
+
+        private void InicializarMapa()
+        {
+            picMapa = new PictureBox();
+            picMapa.Dock = DockStyle.Fill;
+            picMapa.SizeMode = PictureBoxSizeMode.Zoom;
+
+            // Ruta CORRECTA como tu ejemplo del sonido
+            string ruta = Path.Combine(Application.StartupPath, "imagenes", "MapaImagenPNG.png");
+            picMapa.Image = Image.FromFile(ruta);
+
+            this.Controls.Add(picMapa);
         }
     }
 }
