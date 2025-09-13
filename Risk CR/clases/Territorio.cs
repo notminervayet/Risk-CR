cfafdb59f3e0a1a9da8a0a76402f4438b72219bf
EYTHAN YOHEL MENDEZ RODRIGUEZ <yoelr086@gmail.com>
2025-09-13 14:28:43 -0600

    cambios clase Territorio.cs

--- Risk CR/clases/Territorio.cs
+++ Risk CR/clases/Territorio.cs
@@ -1,12 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
 
-namespace Risk_CR.clases
+namespace Risk_CR  // ← Quita el ".clases" 
 {
-    internal class Territorio
+    public class Territorio  // ← Cambia "internal" por "public"
     {
+        // Agrega estas propiedades:
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Provincia { get; set; }
+        public int Tropas { get; set; }
+        public object Ocupante { get; set; }
+        public Button BotonAsociado { get; set; }
+
+        // Agrega este constructor:
+        public Territorio(int id, string nombre, string provincia)
+        {
+            Id = id;
+            Nombre = nombre;
+            Provincia = provincia;
+            Tropas = 0;
+            Ocupante = null;
+            BotonAsociado = null;
+        }
+
+        // Agrega este método:
+        public void ActualizarVisualmente()
+        {
+            if (BotonAsociado != null)
+            {
+                BotonAsociado.Text = $"{Nombre}\n{Tropas}";
+                BotonAsociado.BackColor = Color.LightGray;
+            }
+        }
     }
 }
