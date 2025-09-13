72a42232deebc6c35c3b471515614a9e8ac9a4f1
EYTHAN YOHEL MENDEZ RODRIGUEZ <yoelr086@gmail.com>
2025-09-12 18:52:59 -0600

    me puse loco

--- Risk CR/Program.cs
+++ Risk CR/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-//como la de cartago
+//como la de cartago hgggaggdgg
 namespace Risk_CR
 {
     internal static class Program
