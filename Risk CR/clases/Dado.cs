dd2501e40be0f6be67c59b491daaa1e52ab4008e
notminervayet <l.bonilla.2@estudiantec.cr>
2025-09-15 18:56:59 -0600

    dados pt1

---
+++ Risk CR/clases/Dado.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Risk_CR.clases
+{
+    public class Dado
+    {
+        private static Random random = new Random();
+        public int Lanzar()
+        {
+            return random.Next(1, 7); 
+        }
+    }
+}
